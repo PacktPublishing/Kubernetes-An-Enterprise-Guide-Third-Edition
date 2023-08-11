ab941de4de04797017b249cf196621d41e7c7fb2
root <root@buffalo.hyper-vplanet.com>
2023-08-11 10:06:03 -0400

    adds

--- chapter5/k8gb-example/k8gb/deploy-k8gb-buf.sh
+++ chapter5/k8gb-example/k8gb/deploy-k8gb-buf.sh
@@ -5,10 +5,9 @@ echo -e "Deploying K8GB Using the Buffalo Values file"
 echo -e "*******************************************************************************************************************"
 tput setaf 3
 
-kubectl create ns k8gb
 helm repo add k8gb https://www.k8gb.io
 helm repo update
-helm install k8gb k8gb/k8gb -n k8gb -f k8gb-buf-values.yaml
+helm -n k8gb upgrade -i k8gb k8gb/k8gb --create-namespace -f k8gb-buf-values.yaml
 
 tput setaf 6
 echo -e "\n \n*******************************************************************************************************************"
