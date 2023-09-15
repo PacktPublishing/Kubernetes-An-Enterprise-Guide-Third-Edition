cf445396ff3855176839d75392a9eb3b2afe7c64
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-09-15 14:23:55 -0400

    fix script

--- chapter8/integration/volumes/create-vault.sh
+++ chapter8/integration/volumes/create-vault.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
-sed "s/IPADDR/$hostip/g" < ./vault-volume-watch.yaml  > /tmp/vault-volume-watch.yaml
-kubectl create -f /tmp//tmp/vault-volume-watch.yaml
+sed "s/IPADDR/$hostip/g" < ./volume-vault-watch.yaml  > /tmp/volume-vault-watch.yaml
+kubectl create -f /tmp/volume-vault-watch.yaml
