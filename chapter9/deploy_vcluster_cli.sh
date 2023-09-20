318e1414c88747bd2ade015ca4d71fdcb904a56f
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-09-20 13:28:40 -0400

    add deploy vcluster cli

---
+++ chapter9/deploy_vcluster_cli.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
