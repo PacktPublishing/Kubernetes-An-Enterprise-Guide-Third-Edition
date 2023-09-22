5f2fedecae77b7384ee9bbd8e7af189cbacd7b3e
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-09-22 10:50:53 -0400

    deploy vcluster openunison

---
+++ chapter9/host/deploy_openunison_vcluster.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+vcluster connect myvcluster -n tenant1
+
+echo "Deploying the Kubernetes Dashboard"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+
+wget https://nexus.tremolo.io/repository/ouctl/ouctl-0.0.11-linux -O /tmp/ouctl
+chmod +x /tmp/ouctl
+
+
+echo "Generating helm chart values to /tmp/openunison-vcluster-values.yaml"
+
+export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
+
+
+sed "s/IPADDR/$hostip/g" < ./openunison-values-vcluster.yaml  > /tmp/openunison-vcluster-values.yaml
+
+echo "Deploying Orchestra"
+/tmp/ouctl install-auth-portal -s /tmp/ldaps  -o $REPO_NAME/openunison-operator -c $REPO_NAME/orchestra -l $REPO_NAME/orchestra-login-portal /tmp/openunison-vcluster-values.yaml kind-cluster01 vcluster_myvcluster_tenant1_kind-cluster01
+
+kubectl create -f - <<EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+   name: ou-cluster-admins
+subjects:
+- kind: Group
+  name: cn=k8s-cluster-admins,ou=Groups,DC=domain,DC=com
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: ClusterRole
+  name: cluster-admin
+  apiGroup: rbac.authorization.k8s.io
+EOF
+
+
+
+echo "OpenUnison is deployed to your vcluster!"
