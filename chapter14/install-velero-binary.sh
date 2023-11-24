18069f316252f27c5fc37988aa8f189370611d7c
Scott Surovich <surovich@gmail.com>
2023-11-24 01:55:03 +0000

    Updates

--- chapter14/install-velero-binary.sh
+++ chapter14/install-velero-binary.sh
@@ -3,38 +3,38 @@
 clear
 tput setaf 5
 echo -e "\n*******************************************************************************************************************"
-echo -e "Downloading etcdctl and extracting contents"
+echo -e "Downloading the Velero Binary"
 echo -e "*******************************************************************************************************************"
 tput setaf 2
-wget https://github.com/etcd-io/etcd/releases/download/v3.5.1/etcd-v3.5.1-linux-amd64.tar.gz
-tar xvf etcd-v3.5.1-linux-amd64.tar.gz
+wget https://github.com/vmware-tanzu/velero/releases/download/v1.12.1/velero-v1.12.1-linux-amd64.tar.gz
 
 tput setaf 5
 echo -e "\n*******************************************************************************************************************"
-echo -e "Moving the KinD Binary to /usr/bin"
+echo -e "Extracting archive"
 echo -e "*******************************************************************************************************************"
 tput setaf 2
-sudo cp etcd-v3.5.1-linux-amd64/etcdctl /usr/bin
+tar xvf velero-v1.12.1-linux-amd64.tar.gz
 
 tput setaf 5
 echo -e "\n*******************************************************************************************************************"
-echo -e "Removing archive and extra files"
+echo -e "Moving Velero binary to /usr/bin"
 echo -e "*******************************************************************************************************************"
 tput setaf 2
-rm etcd-v3.5.1-linux-amd64.tar.gz
-rm -rf etcd-v3.5.1-linux-amd64
+sudo mv velero-v1.12.1-linux-amd64/velero /usr/bin
 
 tput setaf 5
 echo -e "\n*******************************************************************************************************************"
-echo -e "Copying the etcd certificates from the control plane into the ./certs directory"
+echo -e "Removing extra files"
 echo -e "*******************************************************************************************************************"
 tput setaf 2
-
-docker cp cluster01-control-plane:/etc/kubernetes/pki/etcd ./certs
+rm -rf velero-v1.12.1-linux-amd64
 
 tput setaf 3
 echo -e "\n*******************************************************************************************************************"
-echo -e "etcdctl download complete"
+echo -e "Velero binary install complete, you should see the version below"
 echo -e "*******************************************************************************************************************"
 tput setaf 2
+velero version
+
+echo -e "\n\n"
 
