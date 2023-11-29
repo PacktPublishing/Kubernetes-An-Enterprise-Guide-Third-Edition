bcdadc0bfb2b07765a6a81d7e34f7a7419d3704e
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-11-29 15:02:43 -0500

    fix script

--- chapter15/simple/deploy-prometheus-charts.sh
+++ chapter15/simple/deploy-prometheus-charts.sh
@@ -5,5 +5,6 @@ helm repo update
 kubectl create ns monitoring
 helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
 
+export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 sed "s/IPADDR/$hostip/g" < ./prometheus-ingress.yaml  > /tmp/prometheus-ingress.yaml
 kubectl create -f /tmp/prometheus-ingress.yaml
