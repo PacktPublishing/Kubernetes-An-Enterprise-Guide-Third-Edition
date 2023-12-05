8df4a476e501b419e6330a7fa141dee047d17e38
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-12-05 10:09:09 -0500

    add alert manager webhook

--- chapter15/simple/deploy-prometheus-charts.sh
+++ chapter15/simple/deploy-prometheus-charts.sh
@@ -8,3 +8,9 @@ helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 sed "s/IPADDR/$hostip/g" < ./prometheus-ingress.yaml  > /tmp/prometheus-ingress.yaml
 kubectl create -f /tmp/prometheus-ingress.yaml
+
+sed "s/IPADDR/$hostip/g" < ./grafana-ingress.yaml  > /tmp/grafana-ingress.yaml
+kubectl create -f /tmp/grafana-ingress.yaml
+
+sed "s/IPADDR/$hostip/g" < ./alertmanager-ingress.yaml  > /tmp/alertmanager-ingress.yaml
+kubectl create -f /tmp/alertmanager-ingress.yaml
