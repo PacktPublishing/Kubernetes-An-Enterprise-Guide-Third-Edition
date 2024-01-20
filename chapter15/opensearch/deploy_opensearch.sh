9a475233ab7d7f3f5d63ae2bed42dc20d1d3a3a7
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-01-20 10:38:51 -0500

    add ns creation

--- chapter15/opensearch/deploy_opensearch.sh
+++ chapter15/opensearch/deploy_opensearch.sh
@@ -20,6 +20,14 @@ tput setaf 3
 
 kubectl patch secret orchestra-secrets-source -n openunison --patch '{"data":{"opensearch-oidc-client-secret":"Qkx4dmw3b01TeDhteHBTR3BPVjBVdFJpS0FyRkxNbTRuMzY4bnpUNGN1OWNYdVZjM0FacTY5dmNQY1EyemV0ag=="}}'
 
+tput setaf 5
+echo -e "\n \n*******************************************************************************************************************"
+echo -e "Pre-deploy OpenSearch"
+echo -e "*******************************************************************************************************************"
+tput setaf 3
+
+kubectl create ns opensearch-cp
+
 
 tput setaf 5
 echo -e "\n \n*******************************************************************************************************************"
@@ -32,3 +40,4 @@ export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 
 sed "s/IPADDR/$hostip/g" < ./opensearch-sso.yaml  > /tmp/opensearch-sso.yaml
 kubectl apply -f /tmp/opensearch-sso.yaml
+
