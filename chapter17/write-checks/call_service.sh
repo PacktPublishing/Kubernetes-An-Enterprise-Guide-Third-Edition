a82e15c786e3c6c5f360bacebf935805dfa8e328
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-02-27 19:09:28 -0500

    fix url

--- chapter17/write-checks/call_service.sh
+++ chapter17/write-checks/call_service.sh
@@ -2,4 +2,4 @@
 
 export hostip=$(hostname  -I | cut -f1 -d' ' | sed 's/[.]/-/g')
 
-curl  -H "Authorization: Bearer $(curl --insecure -u 'mmosley:start123' https://k8sou.$hostip.nip.io/get-user-token/token/user 2>/dev/null| jq -r '.token.id_token')" http://write-checks.$hostip.nip.io/write-check  2>/dev/null  | jq -r
+curl  -H "Authorization: Bearer $(curl --insecure -u 'mmosley:start123' https://k8sou.apps.$hostip.nip.io/get-user-token/token/user 2>/dev/null| jq -r '.token.id_token')" http://write-checks.$hostip.nip.io/write-check  2>/dev/null  | jq -r
